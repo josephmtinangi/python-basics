215fa93253c6b136ff7f5db20ddf9d892211c3ee
Joseph Mtinangi <josephmtinangi@gmail.com>
2017-06-10 11:21:18 +0300

    Lists

---
+++ lists.py
@@ -0,0 +1,14 @@
+# Lists
+
+# Creating lists
+
+computers = ['HP', 'DELL', 'Mac', 'Samsung'];
+
+print(computers)
+
+# Append to list
+computers.append('Advent')
+
+print('After appending')
+
+print(computers)
