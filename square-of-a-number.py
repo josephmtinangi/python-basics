7795166d17e8d4fa8911e3e96a7fecef0f3deff2
Joseph Mtinangi <josephmtinangi@gmail.com>
2017-06-10 11:03:41 +0300

    Write code comments

--- square-of-a-number.py
+++ square-of-a-number.py
@@ -1,3 +1,8 @@
+# This program prompt a user to enter a number
+# Then the program computes the square of that number
+# After computing the square of that number, it determines
+# whether the result is greater than 100 or greater than 50 or otherwise
+
 number = int(input('Enter a number.\n'))
 
 numberSquare = number * number
