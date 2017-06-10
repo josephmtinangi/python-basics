d4e49ccc2548064b30e7cbe251af566559ee5b75
Joseph Mtinangi <josephmtinangi@gmail.com>
2017-06-10 11:25:33 +0300

    Dictionary

---
+++ dictionary.py
@@ -0,0 +1,16 @@
+# Dictionary
+
+person = {'name': 'Joseph Mtinangi', 'college': 'CIVE'}
+
+# Print the dictionary
+print(person)
+
+# Print all keys
+print('Keys')
+print(person.keys())
+
+print('Values')
+print(person.values())
+
+print('All')
+print(person.items())
