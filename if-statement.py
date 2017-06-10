900d114072389a02b80a9a2cc199400528e55a7b
Joseph Mtinangi <josephmtinangi@gmail.com>
2017-06-10 10:58:48 +0300

    Show If statement

---
+++ if-statement.py
@@ -0,0 +1,8 @@
+age = int(input("How old are you?\n"))
+
+if age > 18:
+    print('\n\nSo you are', age)
+    print('\n\nYou can obtain a driving licence')
+else:
+    print('\n\nSo you are', age)
+    print('\n\nWait until you are 18 to obtain a driving licence')
